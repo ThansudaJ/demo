beda8ba723ab9eb62fef2d83fc9acc7dbc161154
ThansudaJ <thansuda.j@bluebik.com>
2024-01-16 14:43:56 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/FaiiJ/helloPrivate"
+	//hello_private "github.com/ThansudaJ/helloPrivate"
+)
+
+func main() {
+	var name = helloPrivate.PrintString("test")
+	fmt.Println(name)
+
+	//var namePrivate = hello_private.PrintString("test")
+	//fmt.Println(namePrivate)
+
+	//var namePrivate = h.PrintString("test")
+	//
+}
